b57249b12d34d0371d8b9710d1c1a52d90905140
chaesang <chaesang@aislestreet.com>
2024-09-30 23:24:07 -0700

    Add stripe payment

--- hello_azure/urls.py
+++ hello_azure/urls.py
@@ -4,4 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('hello', views.hello, name='hello'),
+    path('standalone_button', views.standalone_button, name='standalone_button'),
+    path('redirect_button', views.redirect_button, name='redirect_button'),
+    path('create-checkout-session', views.create_checkout_session, name='redirect_button'),
+    path('success', views.success, name='success'),
+    path('cancel', views.cancel, name='cancel'),
+    
 ]
--- hello_azure/views.py
+++ hello_azure/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.urls import reverse
+from django.http import JsonResponse
 
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 
+import stripe
+import os
+
 def index(request):
     print('Request for index page received')
     return render(request, 'hello_azure/index.html')
+
+def standalone_button(request):
+    print('Payment')
+    return render(request, 'hello_azure/standalone button.html')
+
+def redirect_button (request):
+    return render(request, 'hello_azure/redirect_button.html')
+
+stripe.api_key = 'sk_test_51Q2xbFHo2S95esHCvZllu2zMvO1z9911eTiCC5wxtNenRjqSOAH5jiXXN2k2MMh350lFp4bXtlHpKiOmDCSfySDo006uCsuZZZ'
 
 @csrf_exempt
 def hello(request):
@@ -64,3 +78,50 @@ def hello(request):
             return render(request, 'hello_azure/hello.html', context)
     else:
         return redirect('index')
+    
+@csrf_exempt
+def create_checkout_session(request):
+    # Replace with your Stripe API key
+    # stripe.api_key = 'sk_test_5102xbFHo2595esHCvZ...mDCSfySD0006uCsuZZZ'
+    # 'pk_test_51Q2xbFHo2S95esHChBBAXVc5S4cLHhoijzrn1mHNQAAYz6spsGh7ZZBNSLCiDhcC0iO81eckipjOuInwFdIJuiTW00ThNqk0au'
+    
+    # domain_url = os.getenv('DOMAIN')
+    domain_url='http://localhost:8000'
+
+    session = stripe.checkout.Session.create(
+        line_items=[
+            {
+                'price_data': {
+                    'currency': 'usd',
+                    'product_data': {
+                        'name': 'T-shirt',
+                    },
+                    'unit_amount': 2000,
+                },
+                'quantity': 1,
+            }
+        ],
+        mode='payment',
+
+        # success_url=reverse('success'),
+        # cancel_url=reverse('cancel'),
+        success_url=domain_url + '/success?session_id={CHECKOUT_SESSION_ID}',
+        cancel_url=domain_url + '/canceled',
+        
+    )
+
+    return redirect(session.url, code=303)
+
+# http://localhost:3000/success?session_id=cs_test_a1mfCVlZiJuQ5OKR1JFIoafDHR19DEGD2M3GSiArUfT1v2fhDXH2xZHKN6
+def success(request):
+    id = request.GET.get('session_id')
+    checkout_session = stripe.checkout.Session.retrieve(id)
+    return JsonResponse(checkout_session)
+
+    
+def cancel(request):
+    sample_data = {
+        "status": "canceled",
+        "message": "This is a sample JSON response with a canceled status.",
+    }
+    return JsonResponse(sample_data)
